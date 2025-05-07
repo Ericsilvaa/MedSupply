6401c664869c09e542918be7c72e54762ec9a63f
Eric <eriicsilva28@gmail.com>
2025-05-06 23:23:55 -0300

    feat(agent): gerar link do WhatsApp com base no agente com menos vendas

--- src/utils/format-whatsapp.ts
+++ src/utils/format-whatsapp.ts
@@ -1,13 +1,28 @@
+import { agents } from '@/constants/agents';
 import { CartItem } from '@/stores/cart-store';
+import { AgentType } from '@/types/agents';
+
+function selectAgent(agents: AgentType[]): AgentType {
+    if (agents.length === 0) {
+        throw new Error('Nenhum agente disponível');
+    }
+
+    const minSales = Math.min(...agents.map((a) => a.salesCount));
+    const leastBusyAgents = agents.filter((a) => a.salesCount === minSales);
+
+    // Em caso de empate, sorteia entre os menos ocupados
+    const index = Math.floor(Math.random() * leastBusyAgents.length);
+    return leastBusyAgents[index];
+}
 
 export function generateWhatsAppMessage(items: CartItem[]) {
     let texto = '*Resumo do Orçamento UNINTA:*\n\n';
     let total = 0;
 
     for (const item of items) {
-        const subtotal = item.total * item.quantidadeCarrinho;
+        const subtotal = item.total * item.cartQuantity;
         total += subtotal;
-        texto += `• ${item.descricao} (${item.quantidadeCarrinho}x) - R$ ${subtotal.toFixed(2)}\n`;
+        texto += `• ${item.description} (${item.cartQuantity}x) - R$ ${subtotal.toFixed(2)}\n`;
     }
 
     texto += `\n*Total:* R$ ${total.toFixed(2)}`;
@@ -15,6 +30,7 @@ export function generateWhatsAppMessage(items: CartItem[]) {
 }
 
 export function generateWhatsAppLink(items: CartItem[]) {
+    const agent = selectAgent(agents);
     const msg = generateWhatsAppMessage(items);
-    return `https://wa.me/558534551200?text=${msg}`;
+    return `https://wa.me/${agent.phone}?text=${msg}`;
 }
